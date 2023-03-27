f2131ed15a2abbca66a467c8c8c48a41d3a74bb2
Marcel Barbosa <marcel.sby.br@gmail.com>
2023-03-27 17:03:54 -0300

    Project cleanup & car movimentation through input

--- Prototype 1/Assets/Scripts/PlayerController.cs
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,18 +4,35 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    private float speed = 15.0f;
+    private float turnSpeed = 35.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        // We'll move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        // Get the player input
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // We move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        // Turn the vehicle only if its moving forward or backward
+        if (forwardInput != 0)
+        {
+            // Make the rear movement feels like in real life driving 
+            float calculatedTurnSpeed = forwardInput < 0 ? turnSpeed * -1 : turnSpeed;
+
+            // Turn the vehicle
+            transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput, Space.Self);
+        }
     }
 }
