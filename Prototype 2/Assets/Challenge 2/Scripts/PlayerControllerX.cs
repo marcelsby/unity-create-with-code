db1c9cbc708836886ab9680179f1fdd47eff3bfa
Marcel Barbosa <marcel.sby.br@gmail.com>
2023-03-30 14:28:46 -0300

    Prototype 2 Challenge 2

---
+++ Prototype 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private float lastDogSpawnCall;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Time.time - lastDogSpawnCall >= 0.5) {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                lastDogSpawnCall = Time.time;
+            }
+        }
+    }
+}
